4b5be0dc6c0a9e3fd7432b476d0adf3000526832
Stu Ratcliffe <stuart.ratcliffe@bionical.com>
2018-09-17 07:31:11 +0100

    Removed Bootstrap + jQuery from webpack vendor config

--- webpack.config.vendor.js
+++ webpack.config.vendor.js
@@ -11,11 +11,8 @@ module.exports = (env) => {
         resolve: { extensions: [ '.js' ] },
         entry: {
             vendor: [
-                'bootstrap',
-                'bootstrap/dist/css/bootstrap.css',
                 'event-source-polyfill',
                 'isomorphic-fetch',
-                'jquery',
                 'vue',
                 'vue-router'
             ],
